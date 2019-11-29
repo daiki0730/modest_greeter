33a71189b2eb5d4a3a3c340c770abd6822627d5d
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-29 16:12:28 +0900

    テンプレートに値を渡す

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -3,6 +3,6 @@ defmodule ModestGreeter.HelloController do
 use ModestGreeter.Web, :controller
 
   def show(conn, _params) do
-    render conn, "show.html"
+    render conn, "show.html", name: "Alice"
   end
 end
